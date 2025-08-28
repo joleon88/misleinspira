8d80618248adf3b8633a8763ba75571a65106fab
Jose Manuel Ortega <joseortegaleon88@gmail.com>
2025-08-28 03:15:29 -0500

    Mejora experienia de usuario redirectTo producto.

--- src/components/SuscriptorModal.tsx
+++ src/components/SuscriptorModal.tsx
@@ -87,11 +87,13 @@ const SubscriptorModal: React.FC<SubscriberModalProps> = ({
       const checkSubscription = async () => {
         setStatus("checking");
         try {
+          const redirectToUrl =
+            window.location.origin + window.location.pathname;
           // Usamos signInWithOtp para verificar si el usuario ya existe y enviar un enlace mágico
           const { error } = await supabase.auth.signInWithOtp({
             email: initialEmail,
             options: {
-              emailRedirectTo: window.location.origin,
+              emailRedirectTo: redirectToUrl,
             },
           });
 
@@ -128,12 +130,14 @@ const SubscriptorModal: React.FC<SubscriberModalProps> = ({
     setStatus("loading");
 
     try {
+      // AÑADIDO: Captura la URL actual para la redirección.
+      const redirectToUrl = window.location.origin + window.location.pathname;
       // PRIMERO: Inicia el flujo de autenticación de Supabase.
       // Supabase enviará el correo electrónico.
       const { error } = await supabase.auth.signInWithOtp({
         email,
         options: {
-          emailRedirectTo: window.location.origin,
+          emailRedirectTo: redirectToUrl,
         },
       });
 
